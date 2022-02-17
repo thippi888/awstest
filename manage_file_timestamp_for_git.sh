4159d5cc78f338cbeca21f203abe818bd13f2eba
thippi888 <thippi888@gmail.com>
2022-02-17 17:40:30 +0900

    ko

--- manage_file_timestamp_for_git.sh
+++ manage_file_timestamp_for_git.sh
@@ -2,7 +2,7 @@
 #
 
 for FILE in $(git ls-files); do
-  TIME=$(git log --pretty=format:%ci n1 $FILE)
+  TIME=$(git log --pretty=format:%ci -n1 $FILE)
   echo $TIME'\t'$FILE
 
   STAMP=$(date -d "$TIME" +"%y%m%d%H%M.%S")
